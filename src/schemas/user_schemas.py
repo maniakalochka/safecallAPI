e1572966348775fef5e04e9afa7faa90bc08861e
maniakalochka <prochorwolochoff@gmail.com>
2024-09-13 12:45:01 +0300

    Добавлены схемы пользователей  для создания пользователя (в том числе админа), для чтения пользователя, для обновления пользователя и обновления кармы

---
+++ src/schemas/user_schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+
+
+class UserCreate(BaseModel):
+    username: str = Field(..., title="Username", max_length=255)
+    hashed_password: str = Field(..., title="Password", max_length=255)
+    karma: int = Field(0, title="Karma", ge=0)
+
+
+class SuperUserCreate(UserCreate):
+    is_admin: bool = Field(False, title="Is Admin")
+
+
+class UserRead(BaseModel):
+    id: int = Field(..., title="User ID")
+    username: str = Field(..., title="Username", max_length=255)
+    karma: int = Field(0, title="Karma", ge=0) 
+
+    class Config:
+        from_attributes = True
+
+
+class UserUpdate(BaseModel):
+    username: str = Field(None, title="Username", max_length=255)
+    hashed_password: str = Field(None, title="Password", max_length=255)
+
+    class Config:
+        from_attributes = True
+
+
+class KarmaUpdate(BaseModel):
+    karma: int = Field(..., title="Karma", ge=0)
+
+    class Config:
+        from_attributes = True  
