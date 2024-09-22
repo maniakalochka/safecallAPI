69a462cbc53c716942dc3e25954c4776dce6f07c
maniakalochka <prochorwolochoff@gmail.com>
2024-09-22 12:15:39 +0300

    косметическое улучшение

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import uvicorn
 import asyncio
 from fastapi import FastAPI
+from src.db.db_config import init_db
 
 from src.routers import user_router
-from src.db.db_config import init_db
 
 app = FastAPI()
 
@@ -13,5 +13,5 @@ app.include_router(user_router.router, prefix="/user", tags=["user"])
 
 
 if __name__ == "__main__":
-    asyncio.run(init_db)
+    asyncio.run(init_db())
     uvicorn.run("main:app", host="localhost", reload=True)
